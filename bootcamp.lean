0efe779675d293f3bdb915bb6dfae76a8d11ac50
nthomas103 <nthomas103@gmail.com>
2016-02-03 14:59:20 -0800

    add bootcamp

---
+++ bootcamp.lean
@@ -0,0 +1,47 @@
+import standard
+
+-- installing Lean Emacs-mode
+
+-- basics of functional programming: fib
+
+-- basics of dependent type theory
+
+-- Curry-Howard correspondence 
+
+section curry_howard
+
+variables (p q : Prop)
+
+example : p → p ∨ q ∨ q := by blast
+
+end curry_howard
+
+-- inductive types: nat, list, eq, relations
+
+  --structures: monoids, semigroups, groups, etc etc
+
+
+-- proving things about math
+
+  -- axioms / axiomatic freedom, computable / noncomputable
+
+
+-- Coq library / Mathematical Components library
+
+
+-- Lean math library
+
+
+-- proving things about programming languages
+
+  -- something something STLC
+
+
+-- quantum lambda calculus / Proto-Quipper
+
+
+-- proving things about programs
+
+
+-- references for each section
+
